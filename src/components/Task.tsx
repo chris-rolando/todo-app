359b8267b0f40cc1ccd8aa123ea358168cddd17e
Chris Rolando <chris.rolando@outlook.com>
2024-04-25 21:50:51 -0600

    first commit

---
+++ src/components/Task.tsx
@@ -0,0 +1,15 @@
+type TaskProps = {
+  task: string;
+  deleteTask: () => void;
+};
+
+export const Task = ({ task, deleteTask }: TaskProps) => {
+  return (
+    <div className="task">
+      <span>{task}</span>
+      <button onClick={deleteTask}>Borrar</button>
+    </div>
+  );
+};
+
+
